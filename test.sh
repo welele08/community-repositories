d9921da1db6e132e8370aef0c3d227f947cac8e6
root <welele@gmail.com>
2018-07-10 16:34:36 +0000

    ajour sark-function.sh en local

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -e /sbin/sark-functions.sh ]; then
+if [ ! -e ./sark-functions.sh ]; then
     echo "sabayon-sark package must be installed"
     exit 1
 fi
 
-. /sbin/sark-functions.sh
+. ./sark-functions.sh
 
 if [ ! -e build.yaml ]; then
     echo "Must be run from a repository directory containing a build.yaml"
@@ -17,8 +17,10 @@ load_env_from_yaml build.yaml
 # Speed up test runs by disabling slow syncs and mirror sorts
 export SKIP_PORTAGE_SYNC="${SKIP_PORTAGE_SYNC:-1}"
 export EQUO_MIRRORSORT="${EQUO_MIRRORSORT:-0}"
-
+export DISTFILES=/usr/portage/distfiles
 # Debug what env vars are being passed to the builder
 printenv | sort
 
+export REPOSITORY_NAME=welele
+build_clean welele
 build_all ${BUILD_ARGS}
