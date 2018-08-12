ff60168a827767360d17f4ff83d372c84a4f6669
root <welele@gmail.com>
2018-08-12 19:36:36 +0000

    suppression doc xcas en fix temporaire

--- test.sh
+++ test.sh
@@ -21,8 +21,8 @@ export DISTFILES=/usr/portage/distfiles
 # Debug what env vars are being passed to the builder
 printenv | sort
 
-export CREATEREPO_PHASE="true"
-export REPOSITORY_NAME=capesos
+#export CREATEREPO_PHASE="true"
+#export REPOSITORY_NAME=capesos
 export CLEAN_PHASE="true"
 export CLEAN_CACHE=1
 build_all ${BUILD_ARGS}
